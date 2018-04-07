f42f439c6c1e2856a204bdba3dc67d0dc35d6b4f
Shuchang Zheng <Winton@Shuchangs-MacBook-Pro.local>
2018-04-07 14:16:50 -0700

    update script

--- scripts/install_script.sh
+++ scripts/install_script.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-sudo pip install virtualenv
+brew install python
+sudo pip3 install virtualenv
 
-export WORK_ENV=./.virtualenvs
-export WORK_ENV_ACTIVATE=./.virtualenvs/bin/activate
+export WORK_ENV=~/.shukerbaker_virtualenvs
+export WORK_ENV_ACTIVATE=~/.shukerbaker_virtualenvs/bin/activate
 mkdir -p $WORK_ENV
 sudo rm -rf $WORK_ENV
 virtualenv $WORK_ENV
 source $WORK_ENV_ACTIVATE
 
-sudo pip install Django==1.10
-sudo pip install mysqlclient==1.3.10
-sudo pip install mock==2.0.0
-sudo pip install freezegun==0.3.8
-sudo pip install pyflakes==1.5.0
-sudo pip install pep8==1.7.0
-sudo pip install requests==2.13.0
+pip3 install Django==2.0.4
+pip3 install mysqlclient==1.3.12
+pip3 install mock==2.0.0
+pip3 install freezegun==0.3.10
+pip3 install pyflakes==1.6.0
+pip3 install pep8==1.7.1
+pip3 install requests==2.18.4
+pip3 install ipython==6.3.1
